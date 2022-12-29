23dceb639c822fca9210b81d5208932c4375cc78
Yaroslav Salmin <yaroslav.shoppertrak@gmail.com>
2022-12-29 21:00:58 +0000

    Home page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,63 @@
-import { Button } from "ui";
+import { createStyles, Overlay, Container, Title, Text } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  hero: {
+    position: 'relative',
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    paddingBottom: theme.spacing.xl * 6,
+    zIndex: 1,
+    position: 'relative',
+
+    [theme.fn.smallerThan('sm')]: {
+      height: 500,
+      paddingBottom: theme.spacing.xl * 3,
+    },
+  },
+
+  title: {
+    color: theme.white,
+    fontSize: 60,
+    fontWeight: 900,
+    lineHeight: 1.1,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 40,
+      lineHeight: 1.2,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: 28,
+      lineHeight: 1.3,
+    },
+  },
+}));
 
 export default function Web() {
+  const { classes } = useStyles();
+
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
+    <div className={classes.hero}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+      <Container className={classes.container}>
+        <Title className={classes.title}>
+          Star Wars fandom project featuring Next.js, Mantine, SWAPI.
+        </Title>
+      </Container>
     </div>
   );
 }
